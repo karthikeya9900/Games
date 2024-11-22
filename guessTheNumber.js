4c9d0e87a1cd0f0422eed631378fab390714dc8b
karthikeya <122973946+karthikeya9900@users.noreply.github.com>
2024-11-22 16:03:41 +0530

    Update guessTheNumber.js

--- guessTheNumber.js
+++ guessTheNumber.js
@@ -1,165 +1,125 @@
-function numberIsTooLowMsg(expectedNumber, attempts) {
+function repeat(char, times) {
+  let repeatedString = "";
 
-  let message = "\t -------------------------------------------------------";
-  message += "\n \t|\t " + expectedNumber + ", Is Too low! Try a higher";
-  message += " number. \t\t|\n \t --------------------------------------";
-  message += "-----------------";
-  console.log(message);
+  for (let index = 0; index < times; index++) {
+    repeatedString += char;
+  }
 
-  return attempts = attempts - 1;
+  return repeatedString;
 }
 
-function numberIsTooHighMsg(expectedNumber, attempts) {
-  let message = "\t -------------------------------------------------------";
-  message += "\n \t|\t " + expectedNumber + ", Is Too high! Try a smaller";
-  message += " number. \t|\n \t --------------------------------------";
-  message += "-----------------";
-  console.log(message);
+function printMessage(message) {
+  console.log(makeFrame(message));
+}
 
-  return attempts = attempts - 1;
+function makeFrame(text) {
+  let message = "\t " + repeat("-", 71);
+  message += "\n \t|\t" + text + "\t\t\t|";
+  message += "\n\t " + repeat("-", 71);
+
+  return message;
 }
 
 function winMesssage(expectedNumber) {
-  console.log(" -------------------------------------------------------");
-  console.log("| \t  🏆 🏆 !! CONGRATULATIONS YOU WIN !! 🏆 🏆     |");
-  console.log("| \t\tBravo! You've nailed it.\t\t|");
-  console.log("| \t\tTHE NUMBER WAS:", expectedNumber, "\t\t\t|");
-  console.log(" -------------------------------------------------------");
 
-  return "";
+  let message = "\n\t " + repeat("-", 55);
+  message += "\n\t| \t  🏆 🏆 !! CONGRATULATIONS YOU WIN !! 🏆 🏆     |";
+  message += "\n\t| \t\tBravo! You've nailed it.\t\t|";
+  message += "\n\t| \t\tTHE NUMBER WAS:" + expectedNumber + "\t\t\t|";
+  message += "\n\t " + repeat("-", 55);
+
+  return message;
 }
 
 function gameOverMessage(expectedNumber) {
-  console.log("\t --------------------------------------------------------");
-  console.log("\t| \t💔 Oh no! You've used all your attempts.\t |");
-  console.log("\t| \t💔 👍 Better luck next time!👍\t\t\t |");
-  console.log("\t| \t THE NUMBER WAS:", expectedNumber, "\t\t\t\t |");
-  console.log("\t --------------------------------------------------------");
 
-  return "";
-}
-
-function invalidInputMsg(guessedNumber) {
-  let message = "\t ----------------------------------------------";
-  message += "\n \t|  " + guessedNumber + " is Invalid input! Please enter a";
-  message += " number.  |\n\t ----------------------------------------------";
+  let message = "\n\t " + repeat("-", 56);
+  message += "\n\t| \t💔 Oh no! You've used all your attempts.\t |";
+  message += "\n\t| \t💔 👍 Better luck next time!👍\t\t\t |";
+  message += "\n\t| \t THE NUMBER WAS:" + expectedNumber + "\t\t\t\t |";
+  message += "\n\t " + repeat("-", 56);
 
   return message;
 }
 
-function confirmWantsToPlayAgain() {
-  if (confirm("\n do you want to play again:")) {
-    startGame((Math.ceil(Math.random() * 100)), 5);
-  }
-
-  return "";
-}
-
-function verifyGuessedNumber(guessedNumber, expectedNumber, attempts) {
+function isGuessLessOrGreater(guessedNumber, expectedNumber, attempts) {
   if (+guessedNumber < expectedNumber) {
-    attempts = numberIsTooLowMsg(guessedNumber, attempts);
+    printMessage(guessedNumber + " Is Too low! Try a higher number.      ");
   }
 
   if (+guessedNumber > expectedNumber) {
-    attempts = numberIsTooHighMsg(guessedNumber, attempts);
+    printMessage(guessedNumber + " Is Too high! Try a smaller number.      ");
   }
-  return attempts;
+
+  return attempts - 1;
 }
 
 function headerMessage(attempts) {
-  let message = "-----------------------------------------------------------";
-  message += "\n\t Take a guess! \t\t(Remaining attempts:", attempts, ")";
+  let message = repeat("-", 75);
+  message += "\n\t Take a guess! \t\t(Remaining attempts:" + attempts + ")";
 
   return message;
 }
 
-function getNumberFromPlayer(attempts) {
-  console.log(headerMessage(attempts));
+function readGuess(attempts) {
 
-  const guess = prompt("Enter Number => ");
+  const guess = prompt("\nEnter Number => ");
 
   if (guess !== "" + +guess) {
-    console.log(invalidInputMsg);
-    getNumberFromPlayer(attempts);
+    printMessage(guess + " is Invalid input! Please enter a number. ");
+    readGuess(attempts);
   }
 
   return guess;
 }
 
-function isAttemptsEqualTo0(attempts, expectedNumber) {
-  if (attempts < 1) {
-    gameOverMessage(expectedNumber);
-    confirmWantsToPlayAgain();
-    return true;
+function validateGuessNumber(attempts, expectedNumber) {
+  while (attempts > 0) {
+    console.log(headerMessage(attempts));
+
+    const guessedNumber = readGuess(attempts);
+
+    if (+guessedNumber === expectedNumber) {
+      console.log(winMesssage(expectedNumber));
+      return attempts;
+    }
+
+    attempts = isGuessLessOrGreater(guessedNumber, expectedNumber, attempts);
   }
 
-  return false;
-}
-
-function isExpectedAndGuessSame(expectedNumber) {
-  winMesssage(expectedNumber);
-
-  if (confirmWantsToPlayAgain) {
-    startGame((Math.ceil(Math.random() * 100)), 5);
-  }
-
-  return "";
-}
-
-function isPlayersGuessCorrect(guessedNumber, expectedNumber) {
-  if (+guessedNumber === expectedNumber) {
-    winMesssage(expectedNumber);
-    confirmWantsToPlayAgain();
-
-    return true;
-  }
-
-  return false;
+  return attempts;
 }
 
 function startGame(expectedNumber, attempts) {
-  let stopGame = false;
-  while (!stopGame) {
-    if (isAttemptsEqualTo0(attempts, expectedNumber)) {
-      stopGame = true;
-      continue;
-    }
+  attempts = validateGuessNumber(attempts, expectedNumber);
 
-    const guessedNumber = getNumberFromPlayer(attempts);
+  if (attempts === 0) {
+    console.log(gameOverMessage(expectedNumber));
+  }
 
-    if (isPlayersGuessCorrect(guessedNumber, expectedNumber)) {
-      stopGame = true;
-      continue;
-    }
-
-    attempts = verifyGuessedNumber(guessedNumber, expectedNumber, attempts);
-    continue;
+  if (confirm("do you want play Again:")) {
+    startGame(Math.ceil(Math.random() * 100), 5);
   }
 }
 
 function welcomeMsg() {
-  console.log("\t\t  ------------------------------------------------------");
-  console.log("\t\t     *** 🏆 🏆 Welcome to Guess the Number! 🏆 🏆 ***");
-  console.log("\t\t  ------------------------------------------------------");
+  let message = "\t\t" + repeat("-", 55);
+  message += "\n\t\t     *** 🏆 🏆 Welcome to Guess the Number! 🏆 🏆 ***";
+  message += "\n\t\t" + repeat("-", 55);
+  message += "\n→ The secret number is between 1 and 100.";
+  message += "\n→ You have 5 attempts to find it.";
 
-  console.log("\n→ The secret number is between 1 and 100.\n");
-  console.log("→ You have 5 attempts to find it.\n");
+  return message;
+
 }
 
 function main() {
-  welcomeMsg();
-
-  const isReady = confirm("would you like to play the game:");
-
-  if (isReady) {
+  if (confirm(welcomeMsg())) {
     const secretNumber = Math.ceil(Math.random() * 100);
-    startGame(secretNumber, 5);
-  } else {
-    console.log("\nThank You For Your Precious Time");
+    startGame(10, 5);
   }
+
+  console.log("\nThank You For Your Precious Time");
 }
 
 main();
-// numberIsTooLowMsg(40, 4);
-// numberIsTooHighMsg(40, 4);
-// console.log(invalidInputMsg("a"));

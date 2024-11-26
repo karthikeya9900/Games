669d9369eda7415c8391e9dd4fc229d106010e96
karthikeya <122973946+karthikeya9900@users.noreply.github.com>
2024-11-26 16:12:30 +0530

    Update SPS.js

--- SPS.js
+++ SPS.js
@@ -1,78 +1,34 @@
 const ROCK = " 🪨 ";//rock = 1
 const SCISSOR = " ✂️ ";//scissor = 2
 const PAPER = " 📄 ";//paper = 3
-const instructionsForInput = "\n ENTER \n 1.for 🪨 \n 2.for ✂️ \n 3.for 📄 \n";
-
-let computerScore = 0;
-let playerScore = 0;
-
-// need to ask about this function and using of global scope for score
-function printAndResetScores() {
-  printMessage("\n  player score:" + playerScore);
-  printMessage("\n  computer score:" + computerScore);
-
-  playerScore = 0;
-  computerScore = 0;
-}
 
 function getEmoji(value) {
   if (value === 1) {
-    return " 🪨 ";
+    return "🪨";
   }
 
-  return value === 2 ? " ✂️ " : " 📄 ";
+  return value === 2 ? " ✂️" : "📄";
 }
 
-function printMessage(message) {
-  console.log(message);
-}
-
-function getComputerChoice() {
+function getComputerInput() {
   return Math.ceil(Math.random() * 3);
 }
 
-function welcomeMsg() {
-  let welcomeMsg = "\n    " + repeat("-", 80) + "  \n   |     \t 🪨 📄 ✂️ ";
-  welcomeMsg += "Welcome to Stone Paper Scissor Game ✂️ 📄 🪨 \t\t    |";
-  welcomeMsg += "\n   |    You Have 5 Atttempts In The Five Attempts You Have";
-  welcomeMsg += " To Beat The Computer     |\n   |\t\t\t\t   👍All The Best 👍";
-  welcomeMsg += "\t\t\t\t    |\n    " + repeat("-", 80);
-
-  return welcomeMsg;
-}
-
-function decideGameWinner() {
-  if (playerScore > computerScore) {
-    return frameWinnerMsg("🏆 🏆 !! CONGRATULATIONS YOU WON !! 🏆 🏆");
-  }
-
-  if (playerScore < computerScore) {
-    return frameWinnerMsg("💔 😢OOPS!! Better Luck Next Time 👍 \t");
-  }
-
-  return frameWinnerMsg("     😋It's A Tie Game😋 \t\t ");
-}
-
-function frameWinnerMsg(text) {
-  let message = "\n  " + repeat("-", 74);
-  message += "\n | \t\t  " + text + " \t \t    | ";
-  message += "\n  " + repeat("-", 74);
+function frameForWinnerMsg(text) { 
+  let message = "\n   " + repeat("-", 74);
+  message += "\n  | \t\t  " + text + " \t \t     | ";
+  message += "\n   " + repeat("-", 74);
 
   return message;
 }
 
-function frameOption(text) {
-  let message = "\n  " + repeat("-", 30);
-  message += "\n |\t" + text + "\t|";
-  message += "\n  " + repeat("-", 30);
-
-  return message;
-}
-
-function frameRoundWinnerMsg(text) {
-  let message = "\n  " + repeat("-", 22);
-  message += "\n |\t" + text + "\t|";
-  message += "\n  " + repeat("-", 22);
+// todo: rename this function name 
+function getWelcomeMsg() {
+  let message = "\n    " + repeat("-", 80) + "  \n   |     \t 🪨 📄 ✂️ ";
+  message += "Welcome to Stone Paper Scissor Game ✂️ 📄 🪨 \t\t    |";
+  message += "\n   |    You Have 5 Atttempts In The Five Attempts You Have";
+  message += " To Beat The Computer     |\n   |\t\t\t\t   👍All The Best 👍";
+  message += "\t\t\t\t    |\n    " + repeat("-", 80);
 
   return message;
 }
@@ -87,57 +43,93 @@ function repeat(char, times) {
   return repeatedString;
 }
 
-function readInputFromUser() {
-  printMessage(repeat("-", 50));
-  printMessage(instructionsForInput);
+function decideGameWinner(playerScore, computerScore) {
+  if (playerScore > computerScore) {
+    return frameForWinnerMsg("🏆 🏆 !! CONGRATULATIONS YOU WON !! 🏆 🏆");
+  }
 
-  const userInput = +prompt("enter your choice:");
+  if (computerScore > playerScore) {
+    return frameForWinnerMsg("💔 😢OOPS!! Better Luck Next Time 👍 ");
+  }
 
+  return frameForWinnerMsg("        😋It's A Tie Game😋         ");
+}
+
+// this function is working no issue with this
+function readUserInput() {
+  const inputInstructions = "\n ENTER \n 1.for 🪨 \n 2.for ✂️ \n 3.for 📄 \n";
+  const message = repeat("-", 50) + "\n" + inputInstructions;
+  const userInput = +prompt(message + "\n enter your choice:");
 
   if (userInput >= 1 && userInput <= 3) {
     return userInput;
   }
 
-  printMessage("Inviad input! enter a valid number:");
-  readInputFromUser();
+  console.log("Inviad input! enter a valid number:");
+  return readUserInput();
 }
 
-function decideRoundWinner(playerWinStatus) {
-  if (playerWinStatus) {
-    playerScore = playerScore + 1;
-    return frameRoundWinnerMsg("player won");
+// this function is working no issue with this
+function decideRoundWinner(playerInput, computerInput) {
+  const inputsPair = playerInput + "" + computerInput;
+  const hasPlayerWon = inputsPair === "12" || inputsPair == "23"
+    || inputsPair === "31";
+
+  if (playerInput === computerInput) {
+    return "Tie";
   }
 
-  computerScore = computerScore + 1;
-  return frameRoundWinnerMsg("computer won");
+  if (hasPlayerWon) {
+    return "player";
+  }
+
+  return "computer";
 }
 
-function displayRoundWinner(playerInput, computerChoice) {
-  printMessage(frameOption("computer choice:" + getEmoji(computerChoice)));
-  printMessage(frameOption("player choice:" + getEmoji(playerInput)));
+function showRoundWinner(plInput, devInput, winner, plScore, devScore) {
+  let message = "\n   " + repeat("-", 76);
+  message += "\n  | player input | computer input |   winner   | player score";
+  message += " | computer score |\n   " + repeat("-", 76);
+  message += "\n  |      " + getEmoji(plInput) + "      |        ";
+  message += getEmoji(devInput) + "      |  " + winner + "  |      ";
+  message += plScore + "       |      " + devScore + "         |";
+  message += "\n   " + repeat("-", 76);
 
-  if (playerInput === computerChoice) {
-    return frameRoundWinnerMsg("It Is A TIE  ");
+  console.log(message);
+}
+
+// need to refactor this function
+function play5Rounds() {
+  let playerScore = 0;
+  let deviceScore = 0;
+
+  for (let noOfTimes = 5; noOfTimes > 0; noOfTimes--) {
+    const playerInput = readUserInput();
+    const deviceInput = getComputerInput();
+    const winner = decideRoundWinner(playerInput, deviceInput);
+
+    if (winner === "player") {
+      playerScore += 1;
+      showRoundWinner(playerInput, deviceInput, " player ",
+        playerScore, deviceScore);
+    }
+
+    if (winner === "computer") {
+      deviceScore += 1;
+      showRoundWinner(playerInput, deviceInput, winner,
+        playerScore, deviceScore);
+    }
+
+    if (winner === "Tie") {
+      showRoundWinner(playerInput, deviceInput, "  Tie   ",
+        playerScore, deviceScore);
+    }
   }
-
-  const scissorHitPaper = playerInput === 2 && computerChoice === 3;
-  const paperHitRock = playerInput === 3 && computerChoice === 1;
-  const rockHitScissor = playerInput === 1 && computerChoice === 2;
-  const playerWinStatus = scissorHitPaper || paperHitRock || rockHitScissor;
-
-  return decideRoundWinner(playerWinStatus);
+  console.log(decideGameWinner(playerScore, deviceScore));
 }
 
 function playRPS() {
-  let noOfTimes = 5;
-
-  while (noOfTimes >= 1) {
-    printMessage(displayRoundWinner(readInputFromUser(), getComputerChoice()));
-    noOfTimes = noOfTimes - 1;
-  }
-
-  printMessage(decideGameWinner());
-  printAndResetScores();
+  play5Rounds();
 
   if (confirm("do you want play again:")) {
     playRPS();
@@ -145,26 +137,13 @@ function playRPS() {
 }
 
 function main() {
-  printMessage(welcomeMsg());
+  console.log(getWelcomeMsg());
 
   if (confirm("do you want play this game:")) {
     playRPS();
   }
 
-  printMessage("Thank You For Your Precious Time");
+  console.log("Thank You For Your Precious Time");
 }
 
 main();
-
-// this function is not completed
-function tableUI() {
-  let table = "\n   " + repeat("-", 90);
-  table += "\n  " + "|    AI Choice    |    Player Choice    |     Winner    |";
-  table += "    AI Score    |    Your Score   |";
-  table += "\n   " + repeat("-", 90);
-  table += "\n  |        🪨       |          ✂️          |       🪨      |";
-  table += "       1        |        0        |";
-  table += "\n   " + repeat("-", 90);
-
-  printMessage(table);
-}
